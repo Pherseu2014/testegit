de22aa1b1af6ae0fcc844571db3c41ab63c4344a
Pedro Henrique <pedrohorlando@gmail.com>
2024-02-28 21:41:39 -0300

    commit 02

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    function calcdobro(){
+        $dobro = $num * 2;
+        return $dobro;
+
+    }
+    function calctrip($num){
+        $triplo = $num * 3;
+        return $triplo;
+    }
+?>
